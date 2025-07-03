b7f99405880f0e85da7079c1fc41f0e94bfaba0e
Bekhzad Khamidullaev <69056685+bekhzad-khamidullaev@users.noreply.github.com>
2025-07-03 12:44:07 +0500

    Fix ASGI application path

--- webapp/celery.py
+++ webapp/celery.py
@@ -1,7 +1,7 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webapp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webapp.config.settings')
 
 app = Celery('webapp')
 app.config_from_object('django.conf:settings', namespace='CELERY')
--- webapp/config/asgi.py
+++ webapp/config/asgi.py
@@ -1,10 +1,10 @@
 import os
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-from .channels_ws.routing import websocket_urlpatterns
-from .channels_ws.middleware import JWTAuthMiddleware
+from webapp.channels_ws.routing import websocket_urlpatterns
+from webapp.channels_ws.middleware import JWTAuthMiddleware
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webapp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webapp.config.settings')
 
 django_asgi_app = get_asgi_application()
 
